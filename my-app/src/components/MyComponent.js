a16de887efef10db0561e691a42afdb1d1322ac2
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2018-12-10 16:49:18 +0200

    fetch articles with category added

--- my-app/src/components/MyComponent.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import higherOrderComponent from './higherOrderComponent';
-const MyComponent = props => <div>{JSON.stringify(props, null, 2)}</div>;
-export default higherOrderComponent(MyComponent);
--- my-app/src/components/higherOrderComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-const higherOrderComponent = WrappedComponent => {
-  return class HigherOrderComponent extends Component {
-    render() {
-      return <WrappedComponent {...this.props} />;
-    }
-  };
-};
-export default higherOrderComponent;
