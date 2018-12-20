5028bbb3aca4875b86dbf768819ef0aa466a096e
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2018-12-20 16:49:18 +0200

    code splitting added

--- foody-adminPage/src/components/App.js
+++ foody-adminPage/src/components/App.js
@@ -1,11 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
+// import Loadable from 'react-loadable';
 
 import NotFoundPage from './Admin/Pages/NotFoundPage';
-import MenuItemPage from './Admin/Pages/Menu/MenuItemPage';
-import AddMenuItemForm from './Admin/Pages/Menu/AddMenuItemForm';
-import MainAdminPage from './Admin/Pages/MainAdminPage';
-import MenuItemsPage from './Admin/Pages/Menu/MenuItemsPage';
+import Loader from './Admin/Loader/Loader';
+// import MenuItemPage from './Admin/Pages/Menu/MenuItemPage';
+// import AddMenuItemForm from './Admin/Pages/Menu/AddMenuItemForm';
+// import MainAdminPage from './Admin/Pages/MainAdminPage';
+// import MenuItemsPage from './Admin/Pages/Menu/MenuItemsPage';
+
+// const AsyncMenuItemsPage = Loadable({
+//   loader: () => import('./Admin/Pages/Menu/MenuItemsPage'/* webpackChunkName: "MenuItemsPage"
+//   */),
+//   loading: () => <h1>Loading...</h1>,
+// });
+
+const AsyncMenuItemsPage = lazy(
+  () => import('./Admin/Pages/Menu/MenuItemsPage'),
+  /* webpackChunkName: "MenuItemsPage"
+   */
+);
+
+// const AsyncMenuItemPage = Loadable({
+//   loader: () => import('./Admin/Pages/Menu/MenuItemPage'/* webpackChunkName: "MenuItemPage"
+//   */),
+//   loading: () => <h1>Loading...</h1>,
+// });
+
+const AsyncMenuItemPage = lazy(
+  () => import('./Admin/Pages/Menu/MenuItemPage'),
+  /* webpackChunkName: "MenuItemPage"
+   */
+);
+
+// const AsyncAddMenuItemForm = Loadable({
+//   loader: () => import('./Admin/Pages/Menu/AddMenuItemForm'/* webpackChunkName: "AddMenuItemForm"
+//   */),
+//   loading: () => <h1>Loading...</h1>,
+// });
+
+const AsyncAddMenuItemForm = lazy(
+  () => import('./Admin/Pages/Menu/AddMenuItemForm'),
+  /* webpackChunkName: "AddMenuItemForm"
+   */
+);
+
+// const AsyncMainAdminPage = Loadable({
+//   loader: () => import('./Admin/Pages/MainAdminPage'/* webpackChunkName: "MenuItemPage"
+//   */),
+//   loading: () => <h1>Loading...</h1>,
+// });
+
+const AsyncMainAdminPage = lazy(
+  () => import('./Admin/Pages/MainAdminPage'),
+  /* webpackChunkName: "MainAdminPage"
+   */
+);
 
 export default class App extends Component {
   state = {};
@@ -14,12 +64,14 @@ export default class App extends Component {
     return (
       <div>
         <Switch>
-          <Route exact path="/menu/add" component={AddMenuItemForm} />
-          <Route exact path="/menu/:id" component={MenuItemPage} />
-          <Route exact path="/menu" component={MenuItemsPage} />
-          <Route exact path="/" component={MainAdminPage} />
+          <Suspense fallback={<Loader />}>
+            <Route exact path="/menu/add" component={AsyncAddMenuItemForm} />
+            <Route exact path="/menu/:id" component={AsyncMenuItemPage} />
+            <Route exact path="/menu" component={AsyncMenuItemsPage} />
+            <Route exact path="/" component={AsyncMainAdminPage} />
 
-          <Route exact path="/NotFoundPage" component={NotFoundPage} />
+            <Route exact path="/NotFoundPage" component={NotFoundPage} />
+          </Suspense>
         </Switch>
       </div>
     );
